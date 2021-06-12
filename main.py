c994d8ea34512f15547a0ebc51845f15c62a67d2
thevic16 <20171408@ce.pucmm.edu.do>
2021-06-12 04:48:57 +0000

    arreglandos problemas liberia de audio

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import speech_recognition as sr
-
+import serial
+import time
 
 r = sr.Recognizer()
+#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#ser.flush()
 
 with sr.Microphone() as source:
-    print('Speak Anything : ')
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
+    while (1):
+        print('Speak Anything : ')
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
 
-    try:
-        text = r.recognize_google(audio, language='es-MX')
-        print('You said: {}'.format(text))
-    except:
-        print('Sorry could not hear')
+        '''
+        with open('speech.wav','wb') as f:
+            f.write(audio.get_wav_data())
+        '''
+
+        try:
+            text = r.recognize_google(audio, language='es-MX')
+            print('You said: {}'.format(text))
+        except:
+            print('Sorry could not hear')
