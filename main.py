8da4125f028a081b50a0e7a1ef969d71c10e42e8
thevic16 <20171408@ce.pucmm.edu.do>
2021-08-02 00:52:29 +0000

    ultima actualizacion

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ with sr.Microphone() as source:
     while(1):
         print('Speak Anything : ')
         r.adjust_for_ambient_noise(source)
-        audio = r.listen(source, phrase_time_limit=8)
+        audio = r.listen(source, phrase_time_limit=4)
         try:
-            text = r.recognize_google(audio, language='es-MX', )
+            text = r.recognize_google(audio, language='es-DO')
             text_str = format(text)
             print('You said: '+text_str)
-            if "Sofía" in text_str and "delante" in text_str:
+            if "Sofía" in text_str and "adelante" in text_str:
                 serialPort.write(b"w \r\n")
-            if "Sofía" in text_str and "derecha" in text_str:
+            elif "Sofía" in text_str and "derecha" in text_str:
                 serialPort.write(b"d \r\n")
-            if "Sofía" in text_str and "atrás" in text_str:
+            elif "Sofía" in text_str and "atrás" in text_str:
                 serialPort.write(b"s \r\n")
-            if "Sofía" in text_str and "izquierda" in text_str:
+            elif "Sofía" in text_str and "izquierda" in text_str:
                 serialPort.write(b"a \r\n")
         except:
             print('Sorry could not hear')
